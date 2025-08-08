3e12e45e299277cb6a671b523a28c2f0ddd68e16
caio carvalho - senai <caio.s.carvalho@aluno.senai.br>
2025-08-08 17:17:24 -0300

    1

---
+++ characters.js
@@ -0,0 +1,75 @@
+// Este arquivo armazena os dados dos personagens do jogo.
+// Você pode adicionar mais personagens aqui seguindo a mesma estrutura.
+
+export const characters = [
+    {
+        name: "Caio",
+        gender: "Masculino",
+        type: "Resenha",
+        corDePele:"Branco",
+        habilidades: ["Programação","Resenhar"],
+        origin: "Parnaíba",
+        firstAppearanceYear: 2024,
+        fotodir: "caio.png"
+    },
+    {
+        name: "Raonny",
+        gender: "Masculino",
+        type: "Resenha",
+        corDePele:"Negro",
+        habilidades: ["Irritar"],
+        origin: "Parnaíba",
+        firstAppearanceYear: 2019,
+        fotodir: "raonny.jpg"
+    },
+    {
+        name: "Guilherme",
+        gender: "Masculino",
+        type: "Resenha",
+        corDePele:"Branco",
+        habilidades: ["Programação","Gnose","Jogos"],
+        origin: "Parnaíba",
+        firstAppearanceYear: 2023,
+        fotodir: "guilherme.jpg"
+    },
+    {
+        name: "Jefferson Transcendido",
+        gender: "???",
+        type: "???",
+        corDePele:"Reluzente...",
+        habilidades: ["???"],
+        origin: "Parnaíba",
+        firstAppearanceYear: 2024,
+        fotodir: "jeffersontransendente.jpg"
+    },
+    {
+        name: "Jefferson",
+        gender: "Masculino",
+        type: "Resenha",
+        corDePele:"Branco",
+        habilidades: ["Basqueteiro","Entrosamento"],
+        origin: "Parnaíba",
+        firstAppearanceYear: 2024,
+        fotodir: "jefferson.jpg"
+    },
+    {
+        name: "Gabriel",
+        gender: "Masculino",
+        type: "Resenha",
+        corDePele:"Negro",
+        habilidades: ["Basqueteiro","Voleiador"],
+        origin: "Parnaíba",
+        firstAppearanceYear: 2024,
+        fotodir: "gabriel.jpg"
+    },
+    {
+        name: "Enzo",
+        gender: "Masculino",
+        type: "Entrosa",
+        corDePele:"Branco",
+        habilidades: ["SNAMBIENTAL","Entrosante"],
+        origin: "Parnaíba",
+        firstAppearanceYear: "???",
+        fotodir: "enzo.jpg"
+    }
+];
